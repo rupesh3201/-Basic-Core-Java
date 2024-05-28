c696abbf821b00f8eab8d13d5c846f75f9cc0ccb
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-05-28 21:52:21 +0530

    my 2nd commit

--- hellow.java
+++ hellow.java
@@ -1,7 +1,14 @@
 class Hello_world
 {
-    public static void main(String[] args) {
-        System.out.println("Hellow World");
+    public void disply()
+        {
+            System.out.println("hellow ");
+
+        }
+    public static void main(String[] args) 
+    {
+        Hello_world h = new Hello_world();
+        h.disply();
         
     }
 }
